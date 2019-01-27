2f66242ab4842a9683c58a581d4cef0d9f42025a
e-landeros <landerosedgard@gmail.com>
2019-01-27 14:46:49 -0800

    create models in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True) #use slugs to fetch blog posts must be unique
+    text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User)
+
+    def __inicode__(self):
+        return self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('blog_post_detail', (),
+            {
+                'slug':self.slug
+            })
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            super(Post, self).save(*args, **kwargs)
+
+class Comment(models.Model):
+    name = models.CharField(max_length=42)
+    email = models.EmailField(max_length=75)
+    website = models.URLField(max_length=200, null=True, blank=True)
+    text = models.TextField()
+    post = models.ForeignKey(Post)
+    created_on = models.DateTimeField(auto_add_now=True)
+
+    def __unicode__(self):
+        return self.text
